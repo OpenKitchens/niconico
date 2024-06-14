71bafa97d81a300e30eae0ea70e4c41561216163
Open Kitchens <123849081+OpenKitchens@users.noreply.github.com>
2024-06-14 13:35:55 +0900

    gg

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/control',
+      name: 'control',
+      component: () => import('../views/ControlVideo.vue')
+    }
+  ]
+})
+
+export default router
